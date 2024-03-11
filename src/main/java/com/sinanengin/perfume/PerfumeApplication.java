8618505dd89e19830af5ce71a62b38de26adb363
sinanengin <sinan.engin.02@gmail.com>
2024-03-11 18:12:47 +0300

    Initial commit

---
+++ src/main/java/com/sinanengin/perfume/PerfumeApplication.java
@@ -0,0 +1,13 @@
+package com.sinanengin.perfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinanengin/perfume/PerfumeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinanengin.perfume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfumeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
